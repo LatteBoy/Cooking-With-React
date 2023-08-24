d2bc29adabecc53a41b1357c6121297c435474c7
Harrison Shih <comedyinlyfe@gmail.com>
2023-08-24 14:15:38 +0700

    added search function for recipelist

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,13 +1,30 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import Recipe from "./Recipe";
 import { RecipeContext } from './App';
 
 export default function RecipeList({ recipes }) {
   const { handleRecipeAdd } = useContext(RecipeContext);
+  const [searchQuery, setSearchQuery] = useState('');
+  const filteredRecipes = recipes.filter(recipe => {
+    return recipe.name.toLowerCase().includes(searchQuery.toLowerCase())
+  });
+
   return (
     <div className='recipe-list'>
         <div>
-        {recipes.map((recipe) => {
+          <label 
+            htmlFor='search'>
+            Search:
+          </label>
+          <input
+            type='text'
+            name='search'
+            id='search'
+            onChange={e => setSearchQuery(e.target.value)}
+          />
+        </div>
+        <div>
+        {filteredRecipes.map((recipe) => {
             return <Recipe key={recipe.id} {...recipe} />;
         })}
         </div>
